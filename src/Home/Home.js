2ad66c3831048c747abfce12f3d8b29ea77483fa
parimalt <parimalt@zignuts.com>
2023-05-24 18:26:52 +0530

    React + Tailwind css

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from "../pages/LandingPage";
+
+const Home = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default Home;
